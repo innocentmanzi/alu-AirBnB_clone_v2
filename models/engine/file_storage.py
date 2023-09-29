6fd87af5d941faed8537ce6bf773c09421d8d49d
Innocent MANZI <116680024+innocentmanzi@users.noreply.github.com>
2023-09-29 11:33:34 +0000

    storages

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
+"""Filestorage class for AirBnB"""
 import json
-import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -21,15 +20,28 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def delete(self, obj=None):
+        """deletes obj from __objects if it's inside
+        Args:
+            obj: given object
+        """
+        if not obj:
+            return
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        if key in self.__objects:
+            del self.__objects[key]
+            self.save()
+
     def all(self, cls=None):
         """returns a dictionary
+        Args:
+            cls: class type to filter return by
         Return:
             returns a dictionary of __object
         """
-        if cls:
-            return {key: obj for (key, obj) in self.__objects.items()
-                    if isinstance(obj, type(cls))}
-        return self.__objects
+        if not cls:
+            return self.__objects
+        return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
 
     def new(self, obj):
         """sets __object to given obj
@@ -60,150 +72,6 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """Deletes obj if it's inside the attribute __objects
-        """
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            if (key, obj) in self.__objects.items():
-                self.__objects.pop(key, None)
-        self.save()
-
     def close(self):
-        """Deserializes the JSON file to objects"""
+        """Reload to deserialize JSON file objects"""
         self.reload()
-
-    def classes(self):
-        """Returns a dictionary of valid classes and their references."""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
-
-    def attributes(self):
-        """Returns the valid attributes and their types for classname."""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
-
-# #!/usr/bin/python3
-# """Filestorage class for AirBnB"""
-# import json
-# from models.base_model import BaseModel
-# from models.user import User
-# from models.state import State
-# from models.city import City
-# from models.amenity import Amenity
-# from models.place import Place
-# from models.review import Review
-
-
-# class FileStorage:
-#     """This class serializes instances to a JSON file and
-#     deserializes JSON file to instances
-#     Attributes:
-#         __file_path: path to the JSON file
-#         __objects: objects will be stored
-#     """
-#     __file_path = "file.json"
-#     __objects = {}
-
-#     def delete(self, obj=None):
-#         """deletes obj from __objects if it's inside
-#         Args:
-#             obj: given object
-#         """
-#         if not obj:
-#             return
-#         key = "{}.{}".format(type(obj).__name__, obj.id)
-#         if key in self.__objects:
-#             del self.__objects[key]
-#             self.save()
-
-#     def all(self, cls=None):
-#         """returns a dictionary
-#         Args:
-#             cls: class type to filter return by
-#         Return:
-#             returns a dictionary of __object
-#         """
-#         if not cls:
-#             return self.__objects
-#         return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
-
-#     def new(self, obj):
-#         """sets __object to given obj
-#         Args:
-#             obj: given object
-#         """
-#         if obj:
-#             key = "{}.{}".format(type(obj).__name__, obj.id)
-#             self.__objects[key] = obj
-
-#     def save(self):
-#         """serialize the file path to JSON file path
-#         """
-#         my_dict = {}
-#         for key, value in self.__objects.items():
-#             my_dict[key] = value.to_dict()
-#         with open(self.__file_path, 'w', encoding="UTF-8") as f:
-#             json.dump(my_dict, f)
-
-#     def reload(self):
-#         """serialize the file path to JSON file path
-#         """
-#         try:
-#             with open(self.__file_path, 'r', encoding="UTF-8") as f:
-#                 for key, value in (json.load(f)).items():
-#                     value = eval(value["__class__"])(**value)
-#                     self.__objects[key] = value
-#         except FileNotFoundError:
-#             pass
-
-#     def close(self):
-#         """Reload to deserialize JSON file objects"""
-#         self.reload()
