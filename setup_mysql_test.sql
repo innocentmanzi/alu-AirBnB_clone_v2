65b6c4d16dde900948d21b9c6496ab7dfd70aa9a
Innocent MANZI <116680024+innocentmanzi@users.noreply.github.com>
2023-10-08 13:18:15 +0000

    changes

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,6 @@
--- Prepares a MySQL Test server for the project.
+-- script that prepares a MySQL server for the project
+-- Creates a database and a user with a password
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
